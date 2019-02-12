e1420d4bd4fbf7ad73eb54d9307c5ee0ea64c5d2
Max Silver <Maxbsilver@gmail.com>
2019-02-12 10:45:49 -0700

    comment sections for work on js
    Co-authored-by: Matthew Kaplan <kaplan.matthew.p@gmail.com>
    Co-authored-by: Jacob Bogart <jacobogart@gmail.com>

---
+++ main.js
@@ -0,0 +1,119 @@
+/* Global Variables */
+
+// Matts
+
+
+
+
+
+
+
+// Jacobs
+
+
+
+
+
+
+
+
+// Maxs
+
+
+
+
+
+
+
+/* Event Listeners */
+
+
+// Matts
+
+
+
+
+
+
+
+// Jacobs
+
+
+
+
+
+
+
+
+// Maxs
+
+
+
+
+
+
+
+/* Functions */
+
+
+// Matts
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Jacobs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Maxs
+
+
+
+
+
+
+
