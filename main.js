b6643659c2ecb0b4f49443293efd0ba5adef8080
Max Silver <Maxbsilver@gmail.com>
2019-02-12 13:15:15 -0700

    local storage
    Co-authored-by: Jacob Bogart <Jacobogart@gmail.com>
    Co-authored-by: Matthew Kaplan <Kaplan.matthew.p@gmail.com>

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
   var cardTitleEl = document.querySelector('.title-input');
   var cardBodyEl = document.querySelector('.body-input');
   var storageEl = document.querySelector('.storage');
+  var qualityEl = document.querySelector('.quality');
 
 
 
@@ -13,19 +14,17 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
 /* Functions */
+function createIdea() {
+  var cardTitleVal = cardTitleEl.value;
+  var cardBodyVal = cardBodyEl.value;
+  var ideas = localStorage.getItem('ideas') || '[]';
+  var newIdea = new Idea(cardTitleVal, cardBodyVal);
+  ideas = JSON.parse(ideas);
+  ideas.push(newIdea);
+}
 
-function generateCard(event) {
+function generateCard(event) { 
   event.preventDefault;
   var cardTitleVal = cardTitleEl.value;
   var cardBodyVal = cardBodyEl.value;
@@ -37,11 +36,12 @@ function generateCard(event) {
         <article class="card-bottom">
           <img class="card-btn" src="image-assets/upvote.svg">
           <img class="card-btn" src="image-assets/downvote.svg">
-          <p class="card-bottom-text">Quality: Swill</p>
+          <p class="card-bottom-text">Quality: <span class="quality">Swill</span></p>
           <img class="card-btn" src="image-assets/delete.svg">
         </article>
       </section>`
   storageEl.insertAdjacentHTML('afterbegin', card);
+  createIdea();
 }
 
 
